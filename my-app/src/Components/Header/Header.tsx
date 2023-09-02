ad44e9ac69d55261d3c0169c62fb2ec57f6d1f09
Stanislav <s_naumovich@list.ru>
2023-09-02 18:04:10 +0300

    Little change

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { theme } from 'antd'
 import { Header } from 'antd/es/layout/layout'
 import { FC } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 import { Logout } from '../../Redux/AuthReducer'
 import { AppDispatch } from '../../Redux/ReduxStore'
@@ -22,7 +23,12 @@ export const HeaderPage: FC = () => {
             <img src="https://www.designmantic.com/images/how-to/circle-image-4.png" alt='Check your internet' />
          </div>
          <div className='HeaderData'>
-            {Auth.id ? <HeaderData id={Auth.id} /> : <div className='HeaderLogin'><NavLink to='/Login'><button>Sign in</button></NavLink> </div>}
+            {Auth.id ?
+             <HeaderData id={Auth.id} /> :
+              <div className='HeaderLogin'>
+               <NavLink to='/Login'><button>Sign in</button></NavLink> 
+               <a href='https://social-network.samuraijs.com' target='_blank'><button>Sign up</button></a>
+               </div>}
          </div>
          {Auth.id && <div className='HeaderButton'><button onClick={() => { dispatch(Logout()) }}>Logout</button></div>}
       </div>
